42d50bdf8d95313010028a7718cb9a5cae518ab6
larafonse <nlarafonseca@csumb.edu>
2020-04-26 20:13:06 -0700

    restructured app and added basic components

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './footer';
+import Header from './header';
+import Note from './note';
+
+function App(){
+    return(<div>
+        <Header/>
+        <Note/>
+        <Footer/>
+    </div>)
+}
+export default App;
---
+++ src/note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Note(){
+    return(<div className='note'>
+        <h1>This is a note title</h1>
+        <p>This s the note content</p>
+    </div>)
+}
+
+
+export default Note;
